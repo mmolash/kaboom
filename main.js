b8bf6e4adc3d23b82f3ad95072f4af7e0afa4111
Michael Molash <molash.michael@gmail.com>
2014-01-30 13:00:49 -0600

    finished basket movement and added prisoner

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.onload = function() {
         var maptip = game.assets['Assets/map.png'];
         var image = new Surface(320, 320);
 
-        for (var j = 0; j < 320; j += 16) {
+        for (var j = 0; j < 80; j += 16) {
             for (var i = 0; i < 320; i +=16) {
                 image.draw(maptip, 0, 0, 16, 16, i, j, 16, 16);
                     // maptip: the preloaded image asset used as the source image
@@ -20,45 +20,47 @@ window.onload = function() {
             }
         }
 
-        bg.image = image;
-        game.rootScene.addChild(bg);
-
-        var thing = new Sprite(62, 89);
-        thing.image = game.assets['Assets/basket.png'];
-        thing.x = 160-31;
-        thing.y = 260-45;
-        thing.frame = 0;
-
-        thing.move = function(direction) {
-            switch(direction) {
-              case "right":
-                if (this.x < 320-62) {
-                    thing.x += 5;
-                }
-                break;
-              case "left":
-                if (this.x > 0) {
-                    thing.x -= 5;
-                }
-                break;
-              default:
-                console.log("not a valid direction");
-                break;
+        for (var j = 80; j < 320; j+=16) {
+            for (var i = 0; i < 320; i +=16) {
+                image.draw(maptip, 16, 0, 16, 16, i, j, 16, 16);
             }
         }
 
-        game.rootScene.addChild(thing);
-        thing.addEventListener(Event.ENTER_FRAME, function() {
-            if (g.input.right) {
-                this.move("right");
-            }
-            if (g.input.left) {
-                this.move("left");
-            }
+        bg.image = image;
+        game.rootScene.addChild(bg);
+
+        var basket = new Sprite(62, 89);
+        basket.image = game.assets['Assets/basket.png'];
+        basket.x = 160-31;
+        basket.y = 260-45;
+        basket.frame = 0;
+
+        var prisoner = new Sprite(32, 66);
+        prisoner.image = game.assets['Assets/prisoner.png'];
+        prisoner.x = 160-16;
+        prisoner.y = 51-33;
+        prisoner.frame = 0;
+
+        game.rootScene.addChild(basket);
+        game.rootScene.addChild(prisoner);
+
+        basket.move = function(x) {
+          targetX = x/2 - 31;
+          if (targetX >= 0 && targetX <= 320-62) {
+              this.x = targetX;
+          } else if (targetX > 320-62) {
+              this.x = 320-62
+          } else {
+              this.x = 0
+          }
+        };
+
+        basket.addEventListener(Event.TOUCH_MOVE, function(event) {
+            this.x = event.x;
         });
 
-        thing.addEventListener(Event.TOUCH_MOVE, function(event) {
-            this.x = event.x;
+        document.addEventListener("mousemove", function(event){
+            basket.move(event.pageX);
         });
     };
     game.start();
